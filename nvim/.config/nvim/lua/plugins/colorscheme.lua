ccac3af0ae634ac9dceff1658ab23300f656249e
Magdiel Martinez <magdieltercero@hotmail.com>
2025-05-26 18:54:58 -0600

    Fix statusline accent color for visual_studio_code theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,5 +2,22 @@ return {
     { "folke/tokyonight.nvim" },
     { "Mofiqul/vscode.nvim" },
     { "ellisonleao/gruvbox.nvim" },
-    { "askfiy/visual_studio_code", lazy = false, opts = {}, },
+    {
+        "askfiy/visual_studio_code",
+        lazy = false,
+        config = function()
+            local vsc_thm = require("visual_studio_code")
+            vsc_thm.setup({
+                mode = "dark",
+                hooks = {
+                    after = function(conf, colors, utils)
+                        require("visual_studio_code.utils").hl.bulk_set({
+                            StatusLine = { fg = "#a6a6a6", bg = "#3a3d41" },
+                            StatusLineNC = { link = "StatusLine" },
+                        })
+                    end,
+                }
+            })
+        end,
+    },
 }
